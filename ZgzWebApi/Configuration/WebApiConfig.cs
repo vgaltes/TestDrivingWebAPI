82fc56d6d8c587442db50b4217a2f8741db5ff95
vigarcia <vigarcia@plainconcepts.com>
2014-02-01 14:56:31 +0100

    Removed XML formatter

--- ZgzWebApi/Configuration/WebApiConfig.cs
+++ ZgzWebApi/Configuration/WebApiConfig.cs
@@ -2,12 +2,17 @@
 {
     using System.Web.Http;
     using System.Web;
+    using System.Linq;
 
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {            
             config.MapHttpAttributeRoutes();
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes
+                .FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
