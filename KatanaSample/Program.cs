ec7909aed6fca77f407ef7ec24daf57e244f122f
vigarcia <vigarcia@plainconcepts.com>
2014-02-02 10:31:53 +0100

    Added diagnostics middleware

---
+++ KatanaSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace KatanaSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+    
+            app.Run(context =>
+            {
+                if ( context.Request.Path.Value == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
