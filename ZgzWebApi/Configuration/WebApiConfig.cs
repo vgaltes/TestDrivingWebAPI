5edc4a491f9c4696fa88a201badb8f0be7e5e372
vigarcia <vigarcia@plainconcepts.com>
2014-02-02 10:10:24 +0100

    Removed xml formatter

--- ZgzWebApi/Configuration/WebApiConfig.cs
+++ ZgzWebApi/Configuration/WebApiConfig.cs
@@ -3,12 +3,16 @@
     using System.Web.Http;
     using System.Web;
     using System.Linq;
+    using System.Web.Http.Cors;
 
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {            
             config.MapHttpAttributeRoutes();
+            
+            var cors = new EnableCorsAttribute("http://example.com", "*", "*");
+            config.EnableCors(cors);
 
             var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes
                 .FirstOrDefault(t => t.MediaType == "application/xml");
